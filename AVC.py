b5e442d1683a747184ba8467a2b5c37e332b70fa
Rémi AUCHATRAIRE <134259855+Remi1A@users.noreply.github.com>
2024-04-03 18:19:19 +0200

    AVC.py

--- AVC.py
+++ AVC.py
@@ -19,15 +19,12 @@ from tkinter import messagebox, ttk
 
 class Dataset:
     def __init__(self, data_path):
-        # Charger les données depuis le fichier CSV en spécifiant le séparateur correct
         self.data = pd.read_csv(data_path, delimiter=',')
         self.X_train, self.X_test, self.y_train, self.y_test = [None] * 4
-        self.label_encoders = {}  # Ajout pour stocker les encodeurs
+        self.label_encoders = {} 
     
     def clean_data(self):
-        # Supprimer les lignes où la colonne 'gender' est marquée 'Other'
         self.data = self.data[self.data['gender'] != 'Other']
-        # Convertir 'N/A' en NaN pour la colonne 'bmi' et supprimer les lignes avec NaN
         self.data['bmi'] = pd.to_numeric(self.data['bmi'], errors='coerce')
         self.data = self.data.dropna(subset=['bmi'])
         self.data = self.data[self.data['smoking_status'] != 'Unknown']
@@ -35,6 +32,7 @@ class Dataset:
         
     def visualize_correlation_matrix(self):
         data_for_correlation = self.data.drop(columns=['id'])
+        data_for_correlation = pd.get_dummies(data_for_correlation, drop_first=True)
         corr_matrix = data_for_correlation.corr()
         plt.figure(figsize=(12, 10))
         sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap='coolwarm')
@@ -56,15 +54,10 @@ class Dataset:
                     print(f'La distribution de {col} ne semble pas suivre une loi normale (rejette H0)\n')
 
     def preprocess_data(self):
-        categorical_features = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
-        for feature in categorical_features:
-            le = LabelEncoder()
-            self.data[feature] = le.fit_transform(self.data[feature])
-            self.label_encoders[feature] = le
-
-        X = self.data.drop(['stroke', 'id'], axis=1).values
+        X = self.data[['age', 'hypertension', 'heart_disease', 'avg_glucose_level']].values
         y = self.data['stroke'].values
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
 
 class Model:
     def __init__(self, dataset):
@@ -103,13 +96,11 @@ class Model:
         for feature, value in zip(['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status'], new_data):
             if feature in self.label_encoders:
                 le = self.label_encoders[feature]
-                # Convertir la valeur en chaîne si ce n'est pas déjà le cas
                 value_str = str(value)
-                # Vérifiez si la valeur est dans les classes connues de l'encodeur
                 if value_str in le.classes_:
                     encoded = le.transform([value_str])[0]
                 else:
-                    # Gérer les valeurs inconnues comme vous le jugez nécessaire
+            
                     encoded = -1  # ou autre code pour valeurs inconnues
                 new_data_encoded.append(encoded)
             else:
@@ -143,27 +134,23 @@ class StrokePredictionGUI:
     def setup_gui(self):
         self.window = tk.Tk()
         self.window.title("Prédiction de risque d'AVC")
-
-        labels = ['Gender', 'Age', 'Hypertension', 'Heart Disease', 'Ever Married', 'Work Type', 'Residence Type', 'Avg Glucose Level', 'BMI', 'Smoking Status']
+    
+        # Mise à jour des labels pour inclure uniquement les variables pertinentes
+        labels = ['Age', 'Hypertension', 'Heart Disease', 'Avg Glucose Level']
         self.entries = {}
-
+    
         for i, label in enumerate(labels):
             tk.Label(self.window, text=label + ':').grid(row=i, column=0)
-            if label in ['Gender', 'Hypertension', 'Heart Disease', 'Ever Married', 'Work Type', 'Residence Type', 'Smoking Status']:
-                self.entries[label] = ttk.Combobox(self.window, state="readonly")
-                self.entries[label].grid(row=i, column=1)
-                self.entries[label]['values'] = ('Male', 'Female') if label == 'Gender' else \
-                                               ('Yes', 'No') if label in ['Hypertension', 'Heart Disease', 'Ever Married'] else \
-                                               ('Private', 'Self-employed', 'Govt_job', 'Children', 'Never_worked') if label == 'Work Type' else \
-                                               ('Urban', 'Rural') if label == 'Residence Type' else \
-                                               ('Formerly smoked', 'Never smoked', 'Smokes')
+            if label in ['Hypertension', 'Heart Disease']:
+                self.entries[label] = ttk.Combobox(self.window, state="readonly", values=('Yes', 'No'))
             else:
                 self.entries[label] = tk.Entry(self.window)
-                self.entries[label].grid(row=i, column=1)
-
+            self.entries[label].grid(row=i, column=1)
+    
         predict_button = tk.Button(self.window, text='Prédire', command=self.make_prediction)
         predict_button.grid(row=len(labels), column=1)
         self.window.mainloop()
+
 
     def collect_responses(self):
         for label, entry in self.entries.items():
@@ -172,31 +159,23 @@ class StrokePredictionGUI:
     def format_user_input(self):
         formatted_input = []
         input_mapping = {
-            'Gender': 'gender',
             'Age': 'age',
             'Hypertension': 'hypertension',
             'Heart Disease': 'heart_disease',
-            'Ever Married': 'ever_married',
-            'Work Type': 'work_type',
-            'Residence Type': 'Residence_type',
             'Avg Glucose Level': 'avg_glucose_level',
-            'BMI': 'bmi',
-            'Smoking Status': 'smoking_status'
         }
-
+    
         for gui_label, model_feature in input_mapping.items():
             value = self.user_input[gui_label]
             if gui_label in ['Hypertension', 'Heart Disease']:
                 value = 1 if value == 'Yes' else 0
-            elif gui_label in ['Age', 'Avg Glucose Level', 'BMI']:
+            else:  # Pour 'Age' et 'Avg Glucose Level'
                 value = float(value)
-            elif model_feature in self.model.label_encoders:
-                le = self.model.label_encoders[model_feature]
-                value = le.transform([value])[0] if value in le.classes_ else le.transform([le.classes_[0]])[0]
             
             formatted_input.append(value)
         
         return formatted_input
+
 
     def make_prediction(self):
         self.collect_responses()
